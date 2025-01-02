9b4bba02d88a00a57c06bc0562fca6b2b30d5edb
affan zuko <affanzuko18@gmail.com>
2025-01-02 17:40:08 +0700

    input form

--- Final Project/View/dashboard.Designer.cs
+++ Final Project/View/dashboard.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Final_Project
+﻿namespace Final_Project.View
 {
-    partial class mainForm
+    partial class dashboard
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // mainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1600, 800);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "mainForm";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "dashboard";
         }
 
         #endregion
     }
 }
-
